0bfecb07352b79409950db5b8675f6af98891b32
xianlong bai <xianlong.bai@tendcloud.com>
2020-06-02 10:23:54 +0800

    测试

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -10,7 +10,9 @@ public class Test {
         list.add(1);
         list.add(2);
         list.add(3);
+        list.add(6);
         list.add(4);
+        list.add(888);
         list.add(5);
         System.out.println(list.get(3));
 
